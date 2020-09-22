9a585c222f6f02fa7dc882f807cee3106f6c3e11
Felix Jen <fjen@felixjen.com>
2020-09-21 20:24:12 -0500

    Renamed to actual name

--- keyboards/fjlabs/Shawn65/bolsa65.c
+++ keyboards/fjlabs/Shawn65/bolsa65.c
@@ -1,16 +1,16 @@
-#include "shawn65.h"
+#include "bolsa65.h"
 
-extern inline void shawn65_caps_led_on(void);
+extern inline void bolsa65_caps_led_on(void);
 
-extern inline void shawn65_caps_led_off(void);
+extern inline void bolsa65_caps_led_off(void);
 
 void led_set_kb(uint8_t usb_led) {
     // put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
 
     if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
-        shawn65_caps_led_on();
+        bolsa65_caps_led_on();
     } else {
-        shawn65_caps_led_off();
+        bolsa65_caps_led_off();
     }
 
     led_set_user(usb_led);
